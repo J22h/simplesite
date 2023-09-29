9fa4ffacd64834e4fb7883a1e72debc9b6eb7d12
user <herathjulian2002@gmail.com>
2023-09-29 00:56:50 -0500

    added a hamburger bar drop-down menu for when browser size is less than 600px, cleaned up my code, and made the site a little bit more readable

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Picture Carousel========================================================================================================================================================= */
 var myIndex = 0;
 carousel();
 
@@ -14,3 +15,12 @@ function carousel() {
   x[myIndex - 1].style.display = "block";
   setTimeout(carousel, 3000);
 }
+/* Responsive Navbar======================================================================================================================================================== */
+/* The code below returns an array. we want the first element as there is only one*/
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+/* the code below, whenever the toggleButton is clicked, we run a function => that toggles the 'active' class for navbarLinks */
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
